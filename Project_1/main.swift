c8eb2ab6e5b3a9b7126d58033cb84b98d2e19597
Kiran Ramakrishnan Nair <kiranramakrishnan1994@gmail.com>
2019-06-18 11:42:28 -0400

    Initial Commit

---
+++ Project_1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Project_1
+//
+//  Created by Kiran Ramakrishnan Nair on 6/18/19.
+//  Copyright © 2019 Kiran Ramakrishnan Nair. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
