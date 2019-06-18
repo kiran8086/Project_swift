07fc2c06b2fe0cc73dca6f17da136d0026a1763b
Kiran Ramakrishnan Nair <kiranramakrishnan1994@gmail.com>
2019-06-18 11:49:09 -0400

    commit 1

--- Project_1/main.swift
+++ Project_1/main.swift
@@ -10,3 +10,13 @@ import Foundation
 //kkk
 print("Hello, World!")
 
+import Foundation
+
+func html_tags(_ tag: String, _ text: String) -> String
+{
+    let opening_tag = "<\(tag)>"
+    let closing_tag = ""
+    return "\(opening_tag)\(text)\(closing_tag)"
+}
+print(html_tags("i", "HTML"))
+print(html_tags("b", "Swift"))
